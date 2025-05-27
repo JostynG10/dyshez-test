493fb118d5fb425d811cc74dc270306364127ec8
Jostyn Gómez Alzate <54630490+JostynG10@users.noreply.github.com>
2025-05-27 17:18:49 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "zngdovnbkulfejgkkqvp.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "rtjsuqyiomhphivrwmrd.supabase.co",
+      },
     ],
   },
 };
